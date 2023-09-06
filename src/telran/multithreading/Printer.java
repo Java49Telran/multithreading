647fbf86922da43556a914135ba1f92fb0ff9105
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-06 21:08:30 +0300

    CW# 44 Printer of symbols, Timer

--- src/telran/multithreading/Printer.java
+++ src/telran/multithreading/Printer.java
@@ -2,20 +2,31 @@ package telran.multithreading;
 
 public class Printer extends Thread {
 	
-	private char symbol;
-	private int nSymbols;
-	public Printer(char symbol, int nSymbols) {
-		this.symbol = symbol;
-		this.nSymbols = nSymbols;
+	private char[] symbols;
+	private boolean running = true;
+
+	public void stopPrinter() {
+		this.running = false;
+	}
+
+	public Printer(String symbols) {
+		
+		this.symbols = symbols.toCharArray();
 	}
 	@Override
 	public void run() {
-		for(int i = 0; i < nSymbols; i++) {
-			System.out.println(symbol);
+		
+		int index = 0;
+		while(running) {
+			System.out.print(symbols[index]);
 			try {
-				sleep(1);
+				sleep(1000);
 			} catch (InterruptedException e) {
-				
+
+				index++;
+				if(index == symbols.length) {
+					index = 0;
+				}
 			}
 		}
 	}
--- src/telran/multithreading/PrinterControllerAppl.java
+++ src/telran/multithreading/PrinterControllerAppl.java
@@ -2,22 +2,30 @@ package telran.multithreading;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
 public class PrinterControllerAppl {
 
-	public static void main(String[] args) throws InterruptedException {
-		Printer printer1 = new Printer('#', 100);
-		Printer printer2 = new Printer('*', 100);
-		Instant start = Instant.now();
-		printer1.start();
-		printer2.start();
-		printer1.join();
-		 //main thread moves to waiting for finishing printer1
-		printer2.join();//main thread moves to waiting for finishing printer2
-		                //in the case printer2 has already finished the main thread doesn't wait
-		System.out.printf("running time is %dMs \n",
-				ChronoUnit.MILLIS.between(start, Instant.now()));
-
-	}
-
+	public static void main(String[] args)   {
+		//Interview question: how to join itself in the main thread
+		Thread.currentThread().interrupt();
+		try {
+			Thread.currentThread().join();
+		} catch (InterruptedException e) {
+			System.out.println("interrupted");
+		}
+	   Printer printer = new Printer(".*#$%&");
+	   Scanner scanner = new Scanner(System.in);
+	   printer.start();
+	   while(true) {
+		   String line = scanner.nextLine();
+		   if(line.equals("q")) {
+			   break;
+		   }
+		   printer.interrupt();
+		   
+	   }
+	   
+	   printer.stopPrinter();
+}
 }
