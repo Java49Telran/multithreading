3b0e8ebaa78fe5d9d46313927c7aa01767570009
Yuragranovsky <yuragranovsky@gmail.com>
2023-09-13 19:21:06 +0300

    HW #45 solution

--- src/telran/multithreading/games/Runner.java
+++ src/telran/multithreading/games/Runner.java
@@ -1,11 +1,17 @@
 package telran.multithreading.games;
 
+import java.time.Instant;
+
 public class Runner extends Thread {
 private Race race;
 private int runnerId;
+private Instant finishTime;
 public Runner(Race race, int runnerId) {
 	this.race = race;
 	this.runnerId = runnerId;
+}
+public int getRunnerId() {
+	return runnerId;
 }
 @Override
 public void run() {
@@ -20,6 +26,16 @@ public void run() {
 		}
 		System.out.println(runnerId);
 	}
-	race.setWinner(runnerId);
+	synchronized(race) {
+		finishTime = Instant.now();
+		finishRace();
+	}
+}
+private void finishRace() {
+	race.getResultsTable().add(this);
+
+}
+public Instant getFinsishTime() {
+	return finishTime;
 }
 }
